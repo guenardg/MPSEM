de126ce7cc79e7e32cc64a4ea140710e1dc44782
guenardg <guillaume.guenard@gmail.com>
2022-01-11 09:32:15 -0500

    Updated dyn. symb. reg. file header

--- src/registerDynamicSymbol.c
+++ src/registerDynamicSymbol.c
@@ -1,13 +1,29 @@
-/********************************************************
- C code to handle directed graphs in the context of
- modelling processes modulating trait evolution along
- phylogeny.
- The code is a work in progress and may therefore contain
- more functionalities than those involved in the functions
- functions called from R.
- Guillaume Guenard - Universite de Montreal - 2010-2014
- Routine registration function
-*********************************************************/
+/*************************************************************************
+ 
+ (c) 2010-2022 Guillaume Guénard
+ Université de Montréal, Montreal, Quebec, Canada
+ 
+ **handles directed graphs in the context of modelling processes modulating**
+ **trait evolution along phylogeny.**
+ 
+ This file is part of MPSEM
+ 
+ MPSEM is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ MPSEM is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with MPSEM.  If not, see <https://www.gnu.org/licenses/>.
+ 
+ Dynamic symbol registration
+ 
+ *************************************************************************/
 
 #include <R.h>
 #include <Rinternals.h>
@@ -17,4 +33,3 @@ void R_init_MPSEM(DllInfo* info) {
   R_registerRoutines(info, NULL, NULL, NULL, NULL);
   R_useDynamicSymbols(info, TRUE);
 }
-
